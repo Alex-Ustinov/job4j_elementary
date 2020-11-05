dabd26f7e83d4400a59a7e6c0e1edf42d8289ddf
18485888 <AAlUstinov@sberbank.ru>
2020-11-05 20:09:33 +0300

    9.2. Checkstyle [#401750]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -6,11 +6,11 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        idea = idea+" But I am a newbie.";
+        idea = idea + " But I am a newbie.";
         System.out.println(idea);
         LocalDate date = LocalDate.now();
         int year = date.getYear();
-        idea = idea+" "+year;
+        idea = idea + " " + year;
         System.out.println(idea);
     }
 }
