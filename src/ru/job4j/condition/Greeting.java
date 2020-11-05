cb28034f19c5c6dbda0ac475fedc12a3b18cde5c
18485888 <AAlUstinov@sberbank.ru>
2020-11-05 19:08:53 +0300

    9.1. String [#401744]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+import java.time.LocalDate;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea+" But I am a newbie.";
+        System.out.println(idea);
+        LocalDate date = LocalDate.now();
+        int year = date.getYear();
+        idea = idea+" "+year;
+        System.out.println(idea);
+    }
+}
