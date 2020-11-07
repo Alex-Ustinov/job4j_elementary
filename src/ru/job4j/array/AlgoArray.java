3bab74f7720798d5c823e2fa56170d575c41d9a5
18485888 <AAlUstinov@sberbank.ru>
2020-11-07 19:46:25 +0300

    6.2.2. Переставить элементы массива [#401720]

---
+++ src/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[0];
+            if (temp < array[i]) {
+                array[i] = temp;
+            }
+        }
+
+        for (int y = 0; y < array.length; y++) {
+            System.out.println(array[y]);
+        }
+    }
+}
