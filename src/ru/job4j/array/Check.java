7666ed5969264c39323c62220c29b3574012b305
18485888 <AAlUstinov@sberbank.ru>
2020-11-08 22:44:04 +0300

    6.3. Массив заполнен true или false [#401704] Refactoring

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -3,10 +3,11 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono (boolean[] data) {
         boolean result = true;
+        boolean temp = data[0];
         for (int i = 1; i < data.length; i++) {
-            boolean temp = data[0];
             if (temp != data[i]) {
                 result = false;
+                break;
             }
         }
         return result;
