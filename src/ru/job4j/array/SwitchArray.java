5522b74c88337567d895545dcc90b3bbc210239a
18485888 <AAlUstinov@sberbank.ru>
2020-11-14 01:20:17 +0300

    3. Перегрузить метод max для трех чисел [#401893]

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -20,8 +20,8 @@ public class SwitchArray {
     public static void main(String[] args) {
         int[] numbers = new int[] {1, 2, 3, 4, 5, 6};
         int[] result = swapBorder(numbers);
-        for (int index = 0; index < rsl.length; index++) {
-            System.out.println(rsl[index]);
+        for (int index = 0; index < result.length; index++) {
+            System.out.println(result[index]);
         }
     }
 }
--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,13 +2,28 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-
         int result = left > right ? left : right;
         return result;
     }
 
+    public static int max(int left, int right, int middle) {
+        int leftToRight = max(left, right);
+        int result = leftToRight > middle ? leftToRight : middle;
+        return result;
+    }
+
+    public static int max(int left, int right, int middle, int last) {
+        int leftToRightToMiddle = max(left, right, middle);
+        int result = leftToRightToMiddle > last ? leftToRightToMiddle : last;
+        return result;
+    }
+
     public static void main(String[] args) {
-       int rsl =  max(44,4);
+       int rsl = max(42, 5);
        System.out.println(rsl);
+        int rsl1 = max(2, 73, 8);
+        System.out.println(rsl1);
+        int rsl2 = max(142, 2, 33, 155);
+        System.out.println(rsl2);
     }
 }
